77b703df579b7793f96097ebee162b76e01d38df
Abhiraj Kumar <kumarshishu99@gmail.com>
2022-06-27 04:27:51 +0530

    Added procfile and .gitignore

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-mongoose.connect('mongodb://localhost:27017/postsDB');
+mongoose.connect('mongodb+srv://admin-abhiraj:Test123@cluster0.wojuy.mongodb.net/postsDB');
 
 const postSchema = {
     postTitle : String,
@@ -58,7 +58,6 @@ app.get("/posts/:postTitle", (req,res) => {
 app.get("/", (req, res) => {
     Post.find((err, foundPosts) => {
         res.render("home", {homeData : homeStartingContent, posts : foundPosts});
-        console.log(foundPosts);
     });
     
 });
@@ -72,12 +71,18 @@ app.post("/compose", (req,res) => {
     post.save((err) => {
         if(!err){
             res.redirect("/");
+        }else{
+            console.log(err);
         }
     });
    
     
 });
 
-app.listen(3000, ()=> {
-    console.log("Listening to port 3000...");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, ()=> {
+    console.log("Listening to port...");
 });
